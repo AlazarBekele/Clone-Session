0d05289611a6d3a404ec2fbc7f690f66017b1bca
AlazarBekele <alazarthe70@gmail.com>
2024-11-15 21:15:34 +0300

    Correctlly Connect Back to Fornt

--- Base/views.py
+++ Base/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import (
+  JobDepartement,
+  Apply
+)
 # Create your views here.
 
 def index (request):
-  
-  return render (request, 'index.html')
+
+  apply = Apply.objects.all()
+
+  context = {
+    'apply' : apply
+  }
+
+  return render (request, 'index.html', context=context)
