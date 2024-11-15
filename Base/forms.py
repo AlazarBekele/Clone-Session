aeab2bf7cfd475c550fa7a8e13fe29c9edea4881
AlazarBekele <alazarthe70@gmail.com>
2024-11-15 21:43:45 +0300

    Create Forms

--- Base/forms.py
+++ Base/forms.py
@@ -1,1 +1,23 @@
-from django.forms import forms
+from django import forms
+from .models import (
+  JobDepartement,
+  Apply
+)
+class InputForm (forms.modelForm):
+  class Meta:
+
+    model = Apply
+    fields = '__all__'
+
+    widgets = {
+      'first_name' : forms.TextInput(attrs={
+        'class' : 'form-control'
+      }),
+      'last_name' : forms.TextInput(attrs={
+        'class' : 'form-control'
+      }),
+      'dob' : forms.DateInput(attrs={
+        'class' : 'form-control',
+        'type' : 'Date'
+      })
+    }
--- Base/views.py
+++ Base/views.py
@@ -3,14 +3,18 @@ from .models import (
   JobDepartement,
   Apply
 )
+from .forms import InputForm
 # Create your views here.
 
 def index (request):
 
   apply = Apply.objects.all()
 
+  forms = InputForm(request.POST)
+
   context = {
-    'apply' : apply
+    'apply' : apply,
+    'forms' : forms
   }
 
   return render (request, 'index.html', context=context)
