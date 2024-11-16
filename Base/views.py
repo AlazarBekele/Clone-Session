add16c97dfc4f883c41fcc5174b3b5b0e67bddf8
AlazarBekele <alazarthe70@gmail.com>
2024-11-16 19:04:41 +0300

    continue Login

--- Base/views.py
+++ Base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_Input
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
 from .forms import InputForm
 from django.contrib import messages
 from .models import (
@@ -8,7 +9,7 @@ from .models import (
 )
 # Create your views here.
 
-@login_Input(login_url='')
+@login_required
 def index (request):
 
   apply = Apply.objects.all()
@@ -28,3 +29,19 @@ def index (request):
   }
 
   return render (request, 'index.html', context=context)
+
+
+def login_check (request):
+  
+  form = InputForm(request.POST or None)
+  if request.method == 'POST':
+    if form.is_valid():
+
+      username = form.cleaned_data.get('username')
+      password = form.cleaned_data.get('password')
+
+      user = authenticate(request, username = username, password = password)
+
+      if user is not None:
+        login(request, user)
+        return redirect ('index')
