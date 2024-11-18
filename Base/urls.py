8b874ad211a0c44850bd9227c66c46141a5eef59
Alazar <alazarthe70@gmail.com>
2024-11-18 09:29:50 -0800

    Succssfully Work Login Page

--- Base/urls.py
+++ Base/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import index
+from .views import (
+    index,
+    login_check
+)
 
 
 urlpatterns = [
-    path ('', index, name='index')
+    path ('', index, name='index'),
+    path ('login/', login_check, name='login')
 ]
--- Base/views.py
+++ Base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from .forms import InputForm
 from django.contrib import messages
 from .models import (
@@ -8,7 +8,7 @@ from .models import (
   Apply
 )
 # Create your views here.
-
+@login_required (login_url='/login/')
 def index (request):
 
   form = InputForm(request.POST or None)
@@ -40,14 +40,14 @@ def login_check (request):
       username = form.cleaned_data.get('username')
       password = form.cleaned_data.get('password')
 
-      user = authenticate(request, username = username, password = password)
-
+      user = authenticate(request, username=username, password=password)
+      
       if user is not None:
         login(request, user)
         return redirect ('index')
       
   context = {
-    'from' : form
+    'form' : form
   }
 
-  return render (request, 'Login_page.html', context=context)
+  return render (request, 'Base/Login_page.html', context)
